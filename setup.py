db5bf47c6de4e6d33878bc5b42edcea3224db68b
Umar Tarek <umartarekbusiness@gmail.com>
2026-05-02 13:51:03 +0300

    Update version import from sundae_theme to qyass_pp

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
 # get version from __version__ variable in sundae_theme/__init__.py
-from sundae_theme import __version__ as version
+from qyass_pp import __version__ as version
 
 setup(
 	name="qyass_pp",
