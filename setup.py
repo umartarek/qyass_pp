ce6dcee800be0774e68b4141332a3d3685bd3fb6
Umar Tarek <umartarekbusiness@gmail.com>
2026-05-02 14:44:53 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
 # get version from __version__ variable in sundae_theme/__init__.py
-from qyass_pp import __version__ as version
+from qyass import __version__ as version
 
 setup(
 	name="qyass",
